b0b7cf424119879dad348124463f98016bb340e6
victorsilent <joaovpbarbosa@gmail.com>
2016-05-31 07:28:55 -0300

    create posts to user, and update index page

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,21 @@
+class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.save
+    redirect_to root_path
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title,:body)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users,:path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+  devise_scope :user do
+    resources :posts
+  end
+
   root to: "pages#index"
 end
